312cb69fd6fc778916e6fd2cd0a28eba5c30df47
Andre Rocha <andrefmrocha@live.com.pt>
2019-05-28 18:15:29 +0100

    Added cmakefiles

---
+++ CMakeFiles/MIEIC_CAl_PROJ.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/andrefmrocha/Projects/MIEIC_CAl_PROJ/Appointment.cpp" "/home/andrefmrocha/Projects/MIEIC_CAl_PROJ/CMakeFiles/MIEIC_CAl_PROJ.dir/Appointment.cpp.o"
+  "/home/andrefmrocha/Projects/MIEIC_CAl_PROJ/Coordinates.cpp" "/home/andrefmrocha/Projects/MIEIC_CAl_PROJ/CMakeFiles/MIEIC_CAl_PROJ.dir/Coordinates.cpp.o"
+  "/home/andrefmrocha/Projects/MIEIC_CAl_PROJ/Graph.cpp" "/home/andrefmrocha/Projects/MIEIC_CAl_PROJ/CMakeFiles/MIEIC_CAl_PROJ.dir/Graph.cpp.o"
+  "/home/andrefmrocha/Projects/MIEIC_CAl_PROJ/GraphViewer/GraphicalInterface.cpp" "/home/andrefmrocha/Projects/MIEIC_CAl_PROJ/CMakeFiles/MIEIC_CAl_PROJ.dir/GraphViewer/GraphicalInterface.cpp.o"
+  "/home/andrefmrocha/Projects/MIEIC_CAl_PROJ/GraphViewer/connection.cpp" "/home/andrefmrocha/Projects/MIEIC_CAl_PROJ/CMakeFiles/MIEIC_CAl_PROJ.dir/GraphViewer/connection.cpp.o"
+  "/home/andrefmrocha/Projects/MIEIC_CAl_PROJ/GraphViewer/graphviewer.cpp" "/home/andrefmrocha/Projects/MIEIC_CAl_PROJ/CMakeFiles/MIEIC_CAl_PROJ.dir/GraphViewer/graphviewer.cpp.o"
+  "/home/andrefmrocha/Projects/MIEIC_CAl_PROJ/Heuristics.cpp" "/home/andrefmrocha/Projects/MIEIC_CAl_PROJ/CMakeFiles/MIEIC_CAl_PROJ.dir/Heuristics.cpp.o"
+  "/home/andrefmrocha/Projects/MIEIC_CAl_PROJ/Schedule.cpp" "/home/andrefmrocha/Projects/MIEIC_CAl_PROJ/CMakeFiles/MIEIC_CAl_PROJ.dir/Schedule.cpp.o"
+  "/home/andrefmrocha/Projects/MIEIC_CAl_PROJ/TestGraph.cpp" "/home/andrefmrocha/Projects/MIEIC_CAl_PROJ/CMakeFiles/MIEIC_CAl_PROJ.dir/TestGraph.cpp.o"
+  "/home/andrefmrocha/Projects/MIEIC_CAl_PROJ/TripMate.cpp" "/home/andrefmrocha/Projects/MIEIC_CAl_PROJ/CMakeFiles/MIEIC_CAl_PROJ.dir/TripMate.cpp.o"
+  "/home/andrefmrocha/Projects/MIEIC_CAl_PROJ/UImain.cpp" "/home/andrefmrocha/Projects/MIEIC_CAl_PROJ/CMakeFiles/MIEIC_CAl_PROJ.dir/UImain.cpp.o"
+  "/home/andrefmrocha/Projects/MIEIC_CAl_PROJ/Utils.cpp" "/home/andrefmrocha/Projects/MIEIC_CAl_PROJ/CMakeFiles/MIEIC_CAl_PROJ.dir/Utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/MIEIC_CAl_PROJ.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MIEIC_CAl_PROJ.dir/TestGraph.cpp.o"
+  "CMakeFiles/MIEIC_CAl_PROJ.dir/GraphViewer/graphviewer.cpp.o"
+  "CMakeFiles/MIEIC_CAl_PROJ.dir/Coordinates.cpp.o"
+  "CMakeFiles/MIEIC_CAl_PROJ.dir/Heuristics.cpp.o"
+  "CMakeFiles/MIEIC_CAl_PROJ.dir/GraphViewer/GraphicalInterface.cpp.o"
+  "CMakeFiles/MIEIC_CAl_PROJ.dir/Graph.cpp.o"
+  "CMakeFiles/MIEIC_CAl_PROJ.dir/Appointment.cpp.o"
+  "CMakeFiles/MIEIC_CAl_PROJ.dir/Schedule.cpp.o"
+  "CMakeFiles/MIEIC_CAl_PROJ.dir/Utils.cpp.o"
+  "CMakeFiles/MIEIC_CAl_PROJ.dir/UImain.cpp.o"
+  "CMakeFiles/MIEIC_CAl_PROJ.dir/TripMate.cpp.o"
+  "CMakeFiles/MIEIC_CAl_PROJ.dir/GraphViewer/connection.cpp.o"
+  "MIEIC_CAl_PROJ.pdb"
+  "MIEIC_CAl_PROJ"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MIEIC_CAl_PROJ.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
